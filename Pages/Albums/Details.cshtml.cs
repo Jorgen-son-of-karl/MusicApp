daf3c8801d2e53fc31558a817df3f34878748b16
Nik Gurgaci <nik.gurgaci@studerande.plushogskolan.se>
2022-01-17 18:32:32 +0100

    Album Details på gång

--- Pages/Albums/Details.cshtml.cs
+++ Pages/Albums/Details.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MusicApp.Data;
+using MusicApp.Models;
 
 namespace MusicApp.Pages.Albums
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext database;
+
+        public DetailsModel(ApplicationDbContext database)
         {
+            this.database = database;
+        }
+
+        public Album Album { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Album = await database.Album.FirstOrDefaultAsync(a => a.ID == id);
+
+            if (Album == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
         }
     }
 }
